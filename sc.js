daddd9bbeb4617465f21393d289ee4a9dc4da05c
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-09-24 23:29:44 +0200

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,1 +1,9 @@
-function getCookieValue(a) { const b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)'); return b ? b.pop() : ''; } document.write ("getCookieValue " + getCookieValue("cookiefirst-consent")); 
+document.write ("<hr> CF CONSENT"); 
+
+function getCookieValue(a) { const b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)'); return b ? b.pop() : ''; } document.write ("cookiefirst-consent: " + getCookieValue("cookiefirst-consent")); 
+
+document.write ("<hr> CF ID"); 
+
+function getCookieValue(a) { const b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)'); return b ? b.pop() : ''; } document.write ("cookiefirst-id: " + getCookieValue("cookiefirst-Id")); 
+
+
