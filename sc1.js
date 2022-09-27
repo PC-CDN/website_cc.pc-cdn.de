2aa3bb817e3c948ce19e209ff475e874c77ebf6a
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-09-27 15:37:17 +0200

    Delete sc1.js

--- sc1.js
+++
@@ -1,9 +0,0 @@
-document.write (""); 
-
-function getCookieValue(a) { const b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)'); return b ? b.pop() : ''; } document.write ("cookiefirst-consent: <p>" + getCookieValue("cookiefirst-consent") + "</p>"); 
-
-document.write ("<hr>"); 
-
-function getCookieValue(a) { const b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)'); return b ? b.pop() : ''; } document.write ("cookiefirst-id: <p>" + getCookieValue("cookiefirst-id") + "</p>"); 
-
-
