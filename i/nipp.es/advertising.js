6d74dac7d0f0386860b87689336091bed501f787
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-09-27 20:14:45 +0200

    Update advertising.js

--- i/nipp.es/advertising.js
+++ i/nipp.es/advertising.js
@@ -1,11 +1,11 @@
-text1m = "advertising Cookies are enabled.1m.";
+text1m = '<span style="color:#FFF">ACE.1M</span>';
 document.getElementById("adsContent.1.mobile").innerHTML = text1m;
 
-text1d = "advertising Cookies are enabled.1d.";
+text1d = '<span style="color:#FFF">ACE.1D</span>';
 document.getElementById("adsContent.1.desktop").innerHTML = text1d;
 
-text2m = "advertising Cookies are enabled.2m.";
+text2m = '<span style="color:#FFF">ACE.2M</span>';
 document.getElementById("adsContent.2.mobile").innerHTML = text2m;
 
-text2d = "advertising Cookies are enabled.2d.";
+text2d = '<span style="color:#FFF">ACE.2D</span>';
 document.getElementById("adsContent.2.desktop").innerHTML = text2d;
