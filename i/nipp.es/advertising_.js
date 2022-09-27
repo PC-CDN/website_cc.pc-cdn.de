97ee34a2a5d84afa1b70499859922808baa38f3e
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-09-27 19:36:13 +0200

    Add files via upload

---
+++ i/nipp.es/advertising_.js
@@ -0,0 +1,26 @@
+
+document.getElementById("adsContent.0.mobile").innerHTML = '<img src="https://www.pc-cdn.eu/cdn/nipp.es/ads/placeholder-banner_mobile.png">';
+
+document.getElementById("adsContent.0.desktop").innerHTML = '<img src="https://www.pc-cdn.eu/cdn/nipp.es/ads/placeholder-banner_desktop.png">';
+</script>
+
+
+
+
+document.getElementById("adsContent.mobile").innerHTML = '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=3&p=288&l=ez&f=ifr&linkID=4bea22640fcbe2f017b7864de3f91b12&t=tipwwwtippsco-21&tracking_id=tipwwwtippsco-21" width="320" height="50" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>';
+
+
+document.getElementById("adsContent.desktop").innerHTML = '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=3&p=26&l=ez&f=ifr&linkID=640954d4ec4bdf23812ba59241f92c6e&t=tipwwwtippsco-21&tracking_id=tipwwwtippsco-21" width="468" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>';
+
+document.getElementById("adsContent.1.mobile").innerHTML = '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=3&p=288&l=ez&f=ifr&linkID=4bea22640fcbe2f017b7864de3f91b12&t=tipwwwtippsco-21&tracking_id=tipwwwtippsco-21" width="320" height="50" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>';
+
+
+document.getElementById("adsContent.1.desktop").innerHTML = '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=3&p=26&l=ez&f=ifr&linkID=640954d4ec4bdf23812ba59241f92c6e&t=tipwwwtippsco-21&tracking_id=tipwwwtippsco-21" width="468" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>';
+
+
+document.getElementById("adsContent.2.mobile").innerHTML = '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=3&p=42&l=ur1&category=de_piv&banner=10QJEG92VW3S57RE9AG2&f=ifr&linkID=e814b58a4505800f285f658dc6905bdd&t=nippes-21&tracking_id=nippes-21" width="234" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe></iframe>';
+
+
+document.getElementById("adsContent.2.desktop").innerHTML = '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=3&p=26&l=ur1&category=de_piv&banner=1031ZXV21Q2E6TDPEZ02&f=ifr&linkID=3d7916d20f44975db97241323ec8ace9&t=nippes-21&tracking_id=nippes-21" width="468" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe></iframe>';
+
+
