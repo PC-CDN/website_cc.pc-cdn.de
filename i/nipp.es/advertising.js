7edbe3ab125584fb559a206b53664d62046cfbee
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-09-28 21:21:28 +0200

    Update advertising.js

--- i/nipp.es/advertising.js
+++ i/nipp.es/advertising.js
@@ -1,11 +1,8 @@
+text2d = 'ACE.2D';
+text2m = 'ACE.2M';
+text1d = 'ACE.1D';
 text1m = 'ACE.1M';
 document.getElementById("adsContent.1.mobile").innerHTML = text1m;
-
-text1d = 'ACE.1D';
 document.getElementById("adsContent.1.desktop").innerHTML = text1d;
-
-text2m = 'ACE.2M';
 document.getElementById("adsContent.2.mobile").innerHTML = text2m;
-
-text2d = 'ACE.2D';
 document.getElementById("adsContent.2.desktop").innerHTML = text2d;
