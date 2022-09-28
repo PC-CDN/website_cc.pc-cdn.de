0ed9f2580431dc3af270f54a321b091254dfbceb
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-09-28 21:59:33 +0200

    Update advertising.js

--- i/nipp.es/advertising.js
+++ i/nipp.es/advertising.js
@@ -1,4 +1,4 @@
-text2d = 'ACE.2D';
+text2d = '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=3&p=288&l=ez&f=ifr&linkID=4bea22640fcbe2f017b7864de3f91b12&t=tipwwwtippsco-21&tracking_id=tipwwwtippsco-21" width="320" height="50" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>';
 text2m = 'ACE.2M';
 text1d = 'ACE.1D';
 text1m = 'ACE.1M';
