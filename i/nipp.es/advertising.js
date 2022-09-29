2d6dbab3b4e7834f04dd60b5d33eec11d5645fcb
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-09-29 13:45:35 +0200

    Update advertising.js

--- i/nipp.es/advertising.js
+++ i/nipp.es/advertising.js
@@ -1,4 +1,4 @@
-text0m = '<img src="https://template.pc-cdn.de/assets/bg-images/onepixel-transparent.png" width=320" height="50" border="0">';
+text0m = '<img src="https://template.pc-cdn.de/assets/bg-images/onepixel-transparent.png" width="320" height="50" border="0">';
 text0d = '<img src="https://template.pc-cdn.de/assets/bg-images/onepixel-transparent.png" width="468" height="50" border="0">';
 text1m = '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=3&p=288&l=ez&f=ifr&linkID=4bea22640fcbe2f017b7864de3f91b12&t=nippes-21&tracking_id=nippes-21" width="320" height="50" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>';
 text2m = '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=3&p=42&l=ur1&category=de_piv&banner=10QJEG92VW3S57RE9AG2&f=ifr&linkID=e814b58a4505800f285f658dc6905bdd&t=nippes-21&tracking_id=nippes-21" width="234" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe></iframe>';
