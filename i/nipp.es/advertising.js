485843b3ddba1c40aef157baf822b44ebd782faa
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-09-29 13:37:25 +0200

    Update advertising.js

--- i/nipp.es/advertising.js
+++ i/nipp.es/advertising.js
@@ -1,7 +1,17 @@
+text0m = '<img src="https://template.pc-cdn.de/assets/bg-images/onepixel-transparent.png" width=320" height="50" border="0">';
+text0d = '<img src="https://template.pc-cdn.de/assets/bg-images/onepixel-transparent.png" width="468" height="50" border="0">';
 text1m = '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=3&p=288&l=ez&f=ifr&linkID=4bea22640fcbe2f017b7864de3f91b12&t=nippes-21&tracking_id=nippes-21" width="320" height="50" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>';
 text2m = '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=3&p=42&l=ur1&category=de_piv&banner=10QJEG92VW3S57RE9AG2&f=ifr&linkID=e814b58a4505800f285f658dc6905bdd&t=nippes-21&tracking_id=nippes-21" width="234" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe></iframe>';
 text1d = '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=3&p=26&l=ez&f=ifr&linkID=640954d4ec4bdf23812ba59241f92c6e&t=nippes-21&tracking_id=nippes-21" width="468" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>';
-text2d = 'ACE.2D';
+text2d = '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=3&p=26&l=ur1&category=de_piv&banner=1031ZXV21Q2E6TDPEZ02&f=ifr&linkID=3d7916d20f44975db97241323ec8ace9&t=nippes-21&tracking_id=nippes-21" width="468" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe></iframe>';
+
+if (document.getElementById('adsContent.0.mobile')) {
+document.getElementById("adsContent.0.mobile").innerHTML = text0m;
+} 
+if (document.getElementById('adsContent.0.desktop')) {
+document.getElementById("adsContent.0.desktop").innerHTML = text0d;
+} 
+
 
 if (document.getElementById('adsContent.1.mobile')) {
 document.getElementById("adsContent.1.mobile").innerHTML = text1m;
@@ -9,6 +19,8 @@ document.getElementById("adsContent.1.mobile").innerHTML = text1m;
 if (document.getElementById('adsContent.1.desktop')) {
 document.getElementById("adsContent.1.desktop").innerHTML = text1d;
 } 
+
+
 if (document.getElementById('adsContent.2.mobile')) {
 document.getElementById("adsContent.2.mobile").innerHTML = text2m;
 } 
