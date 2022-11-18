6e672fbaa25aae3a22797557946dfb5d0c4f4aa5
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-11-18 20:14:10 +0100

    Update functional.js

--- all/functional.js
+++ all/functional.js
@@ -3,16 +3,19 @@ if (userLang == "de-DE")
 ulbcontinent = "Europa";
 ulbcountry = "Deutschland";
 ulbamzntridnumber = "21";
+ulbamzndom = "de";
 } else if (userLang == "de")
 {
 ulbcontinent = "Europa";
 ulbcountry = "undefined";
 ulbamzntridnumber = "21";
+ulbamzndom = "de";
 } else if (userLang == "en-US")
 {
 ulbcontinent = "Nordamerika";
 ulbcountry = "USA";
 ulbamzntridnumber = "20";
+ulbamzndom = "com";
 } else 
 {
 ulbcontinent = "undefined";
