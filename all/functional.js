400a819da6954cba82b7e42eba7cfc614ffc0ccc
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-11-18 19:44:17 +0100

    Update functional.js

--- all/functional.js
+++ all/functional.js
@@ -1,1 +1,50 @@
+if (userLang == "de-DE")
+{
+ulbcontinent = "Europa";
+ulbcountry = "Deutschland";
+ulbamzntridnumber = "21";
+} else if (userLang == "en-US")
+{
+ulbcontinent = "Nordamerika";
+ulbcountry = "USA";
+ulbamzntridnumber = "20";
+} else 
+{
+ulbcontinent = "undefined";
+ulbcountry = "undefined";
+ulbamzntridnumber = "20";
+}
+/*
+if (userLang == "")
+{
+ulbcontinent = "";
+ulbcountry = "";
+ulbamzntridnumber = "";
+}
+if (userLang == "")
+{
+ulbcontinent = "";
+ulbcountry = "";
+ulbamzntridnumber = "";
+}
+if (userLang == "")
+{
+ulbcontinent = "";
+ulbcountry = "";
+ulbamzntridnumber = "";
+}
+if (userLang == "")
+{
+ulbcontinent = "";
+ulbcountry = "";
+ulbamzntridnumber = "";
+}
+if (userLang == "")
+{
+ulbcontinent = "";
+ulbcountry = "";
+ulbamzntridnumber = "";
+}
+*/
+
 
