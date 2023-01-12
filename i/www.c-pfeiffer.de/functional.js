960e9b2ec29fe3468549b1acf20869cf87622ba8
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2023-01-12 22:43:10 +0100

    Add files via upload

---
+++ i/www.c-pfeiffer.de/functional.js
@@ -0,0 +1,4 @@
+function getCookieValue(a) { const b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)'); return b ? b.pop() : ''; } console.log ("getCookieValue " + getCookieValue("favorite-dark-mode "));
+
+ 
+
